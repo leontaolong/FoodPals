0d1a8d43b7ba1a1e1c3a38990d30abd01c8babf4
leontaolong <longt8@uw.edu>
2018-03-01 12:57:30 -0800

    Design and add multiple handlers

--- server/handlers/handlers.js
+++ server/handlers/handlers.js
@@ -7,9 +7,49 @@ const express = require('express');
 module.exports = function (userStore, postStore) {
     //create a new Mux
     let router = express.Router();
-
-    router.get('/v1/user', (req, res, next) => {
+    
+    // add new user with user info
+    router.post('/v1/user', (req, res, next) => {
         
+    });
+
+    // get specific user info by id
+    router.get('/v1/user', (req, res, next) => {
+
+    });
+
+    // add a post with post info
+    // respond with matched appointment if find one, otherwise respond descriptive text
+    router.post('/v1/post', (req, res, next) => {
+        
+    });
+
+    // update a post with post info
+    // respond with matched appointment if find one, otherwise respond descriptive text
+    router.put('/v1/post', (req, res, next) => {
+        
+    });
+
+    // delete a post by post id
+    router.delete('/v1/post', (req, res, next) => {
+            
+    });
+
+    // get all existing posts
+    router.get('/v1/posts', (req, res, next) => {
+    
+    });
+
+    // request matching for a post by user id and post id
+    // respond with descriptive text 
+    router.post('/v1/request', (req, res, next) => {
+            
+    });
+
+    // respond a match request with user id and post id
+    // respond with matched appointment if find one, otherwise respond descriptive text  
+    router.post('/v1/response', (req, res, next) => {
+            
     });
 
     return router;
