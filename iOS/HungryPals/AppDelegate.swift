9787703fa64186d69f90fe1585529c3d3e24cdaa
Christy Lu <hyl9@uw.edu>
2018-03-02 13:16:22 -0800

    facebook login done

--- iOS/HungryPals/AppDelegate.swift
+++ iOS/HungryPals/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import FacebookCore
+import FacebookLogin
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,11 +17,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        SDKApplicationDelegate.self.shared.application(application, didFinishLaunchingWithOptions: launchOptions)
+        
         return true
     }
+    
+    func application (_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+        return SDKApplicationDelegate.shared.application(app, open: url, options: options)
+    }
 
-    func applicationWillResignActive(_ application: UIApplication) {
+    /*func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
     }
@@ -39,7 +47,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
+    }*/
 
 
 }
---
+++ iOS/HungryPals/LoginViewController.swift
@@ -0,0 +1,74 @@
+//
+//  LoginViewController.swift
+//  HungryPals
+//
+//  Created by Christy Lu on 3/1/18.
+//  Copyright © 2018 UW iSchool. All rights reserved.
+//
+
+import UIKit
+import FacebookCore
+import FacebookLogin
+
+class LoginViewController: UIViewController {
+    @IBOutlet weak var facebookInfo: UILabel!
+    
+    
+    @IBAction func btnLogin(_ sender: UIButton) {
+        let loginManager = LoginManager()
+        loginManager.logIn(readPermissions: [.publicProfile, .email], viewController: self) { result in
+            switch result {
+            case .failed(let error):
+                print(error)
+            case .cancelled:
+                print("user cancelled the login")
+            case .success(_, _, _):
+                self.getUserInfo { userInfo, error in
+                    if let error = error { print(error.localizedDescription)}
+                    if let userInfo = userInfo, let id = userInfo["id"], let name = userInfo["name"], let email = userInfo["email"] {
+                        self.facebookInfo.text = "ID: \(id), Name: \(name), Email: \(email)"
+                    }
+                    if let userInfo = userInfo, let pictureUrl = ((userInfo["picture"] as? [String: Any])?["data"] as? [String: Any])?["url"] as? String {
+                        print("pictureUrl: \(pictureUrl)")
+                    }
+                }
+            }
+        }
+    }
+    
+    
+    func getUserInfo(completion: @escaping (_:[String: Any]?, _:Error?) -> Void) {
+        let request = GraphRequest(graphPath: "me",parameters: ["fields": "id, name, email, picture"])
+        request.start { response, result in
+            switch result {
+            case .failed(let error):
+                completion(nil, error)
+            case .success(let graphResponse):
+                completion(graphResponse.dictionaryValue, nil)
+            }
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
