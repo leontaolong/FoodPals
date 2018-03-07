e8d4eb94d58064ae474e37c1600e5052a85fdd45
Jeffrey Xu <jeffreyxrh@gmail.com>
2018-03-07 15:44:04 -0800

    implemented PostsView code

--- iOS/HungryPals/PostViewController.swift
+++ iOS/HungryPals/PostViewController.swift
@@ -13,11 +13,13 @@ class PostViewController: UIViewController, UITableViewDataSource, UITableViewDe
     
     let postRepo = PostRepository.shared
     var posts: [Post]? = nil
+    var users: [User]? = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         posts = UIApplication.shared.postRepository.getPosts()
+        users = UIApplication.shared.postRepository.getUsers()
         
         // Do any additional setup after loading the view.
         postsTable.dataSource = self
@@ -38,9 +40,17 @@ class PostViewController: UIViewController, UITableViewDataSource, UITableViewDe
         let post = posts![index]
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "LabelCell", for: indexPath)
-        cell.textLabel?.text = post.getRestaurant()
-        //cell.detailTextLabel?.text = post.desc
-        //cell.imageView?.image = UIImage(named: post.image)
+        cell.textLabel?.text = post.getCreator().getUsername()
+        cell.detailTextLabel?.text = "Wants to eat \(post.getRestaurant())\n At \(post.getStartTime().toString()) - \(post.getEndTime().toString()), Today"
+        
+        let imageUrl = URL(string: post.getCreator().getProfilePic())!
+        
+        let imageData = try! Data(contentsOf: imageUrl)
+        
+        //let image = UIImage(data: imageData)
+        
+        cell.imageView?.image = UIImage(data: imageData)
+        //cell.imageView?.image = UIImage(named: post.getCreator().getProfilePic())
         return cell
     }
     
