87d0b77c80280f917b8e8c19989c832de31675da
huangjoyce3 <jhuang3@uw.edu>
2018-03-07 20:59:32 -0800

    clean 3rd view controller

--- iOS/HungryPals/ThirdViewController.swift
+++ iOS/HungryPals/ThirdViewController.swift
@@ -8,57 +8,16 @@
 
 import UIKit
 
-class ThirdViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource, UITextFieldDelegate  {
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return cuisines.count
-    }
-
-    @IBOutlet weak var pickerView: UIPickerView!
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
-    }
-
-
-    @IBOutlet weak var cuisine: UITextField!
-    @IBOutlet weak var restaurant: UITextField!
-    @IBOutlet weak var time: UITextField!
-    @IBOutlet weak var numberPeople: UITextField!
-    @IBOutlet weak var notes: UITextField!
-
+class ThirdViewController: UIViewController{
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        let pickerView = UIPickerView()
-        pickerView.delegate = self
+        // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    @IBOutlet weak var pickerTextField : UITextField!
-
-    let cuisines = ["Japanese", "Chinese", "American", "Greek"]
-
-    // Sets number of columns in picker view
-    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
-        return 1
-    }
-
-    // Sets the number of rows in the picker view
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return cuisines[row]
-    }
-
-    // This function sets the text of the picker view to the content of the "salutations" array
-//    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-//        return cuisines[row]
-//    }
-
-    // When user selects an option, this function will set the text of the text field to reflect
-    // the selected option.
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        cuisine.text = cuisines[row]
-    }
+    
 }
