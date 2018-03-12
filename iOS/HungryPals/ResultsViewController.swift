bc6f2d61b3c8d099557aa7a06be9d02aff6c6e57
huangjoyce3 <jhuang3@uw.edu>
2018-03-12 15:07:31 -0700

    adding messenger

--- iOS/HungryPals/ResultsViewController.swift
+++ iOS/HungryPals/ResultsViewController.swift
@@ -17,7 +17,7 @@ class ResultsViewController: UIViewController {
     @IBOutlet weak var nameLabel: UILabel!
     
     //TODO: change to actual notification
-    var notification = Post.init(postId: "12381379", creator: User.init(username: "JOyce", email: "String", location: "String", userId: "String", profilePic: "https://scontent.fsea1-1.fna.fbcdn.net/v/t1.0-1/p320x320/27073113_546712599021153_8141215265311775044_n.jpg?oh=6c3ab12fe2ba078a7598e26c7264f2a6&oe=5B0B66E1", deviceToken: "String"), createdAt: Date(timeIntervalSinceReferenceDate: -123456789.0), status: "PENDING", startTime: Date(timeIntervalSinceReferenceDate: -123456789.0), endTime: Date(timeIntervalSinceReferenceDate: -123456389.0), restaurant: "none", cuisine: "Chinese", notes: "I don't like spice")
+    var notification = Post.init(postId: "12381379", creator: User.init(username: "JOyce", email: "String", location: "String", userId: "String", profilePic: "https://scontent.fsea1-1.fna.fbcdn.net/v/t1.0-1/p320x320/27073113_546712599021153_8141215265311775044_n.jpg?oh=6c3ab12fe2ba078a7598e26c7264f2a6&oe=5B0B66E1", deviceToken: "String"), createdAt: Date(timeIntervalSinceReferenceDate: -123456789.0), status: "CONFIRMED", startTime: Date(timeIntervalSinceReferenceDate: -123456789.0), endTime: Date(timeIntervalSinceReferenceDate: -123456389.0), restaurant: "none", cuisine: "Chinese", notes: "I don't like spice")
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,9 +33,29 @@ class ResultsViewController: UIViewController {
         notesLabel.text = "\(notification.notes)"
     }
     
-    @IBAction func eatTogetherBtn(_ sender: UIButton) {
+        @IBAction func eatTogetherBtn(_ sender: UIButton) {
         print("eat together")
-        
+            
+        //TODO: Open Messenger when status is CONFIRMED
+        if notification.status == "CONFIRMED" {
+            let alertVC = UIAlertController(title: "Woohoo! Not lonely anymore!",
+                                            message: "Want to open Messenger to chat with \(notification.creator.username)?",
+                                            preferredStyle: .alert)
+            alertVC.addAction(UIAlertAction(title: "Back", style: UIAlertActionStyle.cancel) { _ in
+                alertVC.dismiss(animated: true)
+            })
+            alertVC.addAction(UIAlertAction(title: "Open", style: .default) { _ in
+                let url  = NSURL(string: "https://messenger.com")
+                let application = UIApplication.shared
+
+                if application.canOpenURL(url! as URL) {
+                    application.open(url! as URL)
+                }
+            })
+            self.present(alertVC, animated: true)
+        }else{
+            //TODO: Redirect to homepage when status is REQUESTED
+        }
     }
     
     @IBAction func noThanksBtn(_ sender: UIButton) {
