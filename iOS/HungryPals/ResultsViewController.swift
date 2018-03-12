e7de3689f0f67c82b25a6543fcf9211d68d33344
huangjoyce3 <jhuang3@uw.edu>
2018-03-12 11:29:13 -0700

    results screen

--- iOS/HungryPals/ResultsViewController.swift
+++ iOS/HungryPals/ResultsViewController.swift
@@ -9,15 +9,30 @@
 import UIKit
 
 class ResultsViewController: UIViewController {
-
+    let dataRepo = DataRepository.shared
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var cuisineLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var notesLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        let imgurl = "https://scontent.fsea1-1.fna.fbcdn.net/v/t1.0-1/p320x320/27073113_546712599021153_8141215265311775044_n.jpg?oh=6c3ab12fe2ba078a7598e26c7264f2a6&oe=5B0B66E1"
+        let url = URL(string: imgurl)
+        let data = try? Data(contentsOf: url!)
+        imageView.image = UIImage(data: data!)
+        imageView.layer.cornerRadius = imageView.frame.height / 2
+        imageView.clipsToBounds = true
         
-        let img = UIImage(named:"IMG_6073")
-        self.imageView.image = img?.crop(rect: CGRect(x: 0, y: 0, width: 50, height: 50))
-        self.imageView.layer.cornerRadius = self.imageView.frame.size.width / 2
-        self.imageView.clipsToBounds = true
+//        nameLabel.text = "\(dataRepo.getUser().username)"
+//        cuisineLabel.text = "\() food"
+//        timeLabel.text = "At \() - \()"
+//        notesLabel.text = "\()"
+        nameLabel.text = "Estelle Liang"
+        cuisineLabel.text = "mexican food"
+        timeLabel.text = "At 12:30pm - 1:30pm"
+        notesLabel.text = "I have gluten allergies"
         
     }
 
@@ -28,16 +43,3 @@ class ResultsViewController: UIViewController {
 
 }
 
-extension UIImage {
-    func crop( rect: CGRect) -> UIImage {
-        var rect = rect
-        rect.origin.x*=self.scale
-        rect.origin.y*=self.scale
-        rect.size.width*=self.scale
-        rect.size.height*=self.scale
-        
-        let imageRef = self.cgImage!.cropping(to: rect)
-        let image = UIImage(cgImage: imageRef!, scale: self.scale, orientation: self.imageOrientation)
-        return image
-    }
-}
